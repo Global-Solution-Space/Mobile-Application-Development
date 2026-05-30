5698838d21283182d897c7a18b2cb80d11d29a96
lunaguima <luna.dcarvalho05@gmail.com>
2026-05-30 16:28:35 -0300

    chore: atualiza dependências e configurações de build para ambiente web

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,15 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Intercepta o Zustand e força ele a usar a versão compatível com a Web (CommonJS)
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName.includes('zustand')) {
+    const result = require.resolve(moduleName);
+    return context.resolveRequest(context, result, platform);
+  }
+  // Para todo o resto, segue a vida normal
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
